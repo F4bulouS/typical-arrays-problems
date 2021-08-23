eb2bb33c64df887a6002b2e465f8fcb791e2e6e9
FabulouS <newbie1690@gmail.com>
2021-08-23 23:58:32 +0400

    feat: add min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+exports.min = function min(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+    let minElement = Number.MAX_VALUE;
+    array.forEach((element) => {
+        minElement = element < minElement ? element : minElement;
+    });
+    return minElement;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+    let maxElement = Number.MIN_VALUE;
+    array.forEach((element) => {
+        maxElement = element > maxElement ? element : maxElement;
+    });
+    return maxElement;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+    return array.reduce((partial_sum, a) => partial_sum + a, 0) / array.length;
+};
